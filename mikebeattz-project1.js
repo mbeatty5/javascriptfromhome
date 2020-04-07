500854d96e16effc507a0a81629ec6843be1866f
mbeatty5 <mbeatty5@gmail.com>
2020-04-06 20:59:44 -0600

    Project 1
    Project 1 from learntocode

---
+++ mikebeattz-project1.js
@@ -0,0 +1,10 @@
+// declare variables
+let firebeats = 'https://soundcloud.com/mbeatty5';
+let luckynumber = 13;
+let covid = null;
+let time = undefined;
+
+// check if string variable is string type
+if (typeof firebeats === 'string') {
+    console.log('hello, string');
+}
